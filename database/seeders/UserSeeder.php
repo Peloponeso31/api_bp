e4b65f36b4f0d6e12b6896c0f1c108f8a97a53e8
peloponeso31 <tanilizquierdo@proton.me>
2025-06-25 17:20:50 -0600

    Upgrade a Laravel 12

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,59 +21,11 @@ class UserSeeder extends Seeder
                 'sexo_id' => 1,
                 'puesto_id' => Puesto::inRandomOrder()->first()->id,
                 'oficina_id' => Oficina::inRandomOrder()->first()->id,
-                'nombre' => 'Jonatan',
-                'apellido_paterno' => 'Luna',
-                'apellido_materno' => 'Franco',
-            ])->id,
-            'email' => 'jon@cebv.com',
-        ]);
-
-        User::factory()->create([
-            'empleado_id' => Empleado::create([
-                'sexo_id' => 1,
-                'puesto_id' => Puesto::inRandomOrder()->first()->id,
-                'oficina_id' => Oficina::inRandomOrder()->first()->id,
                 'nombre' => 'Tanil',
                 'apellido_paterno' => 'Izquierdo',
                 'apellido_materno' => 'Cordova',
             ])->id,
-            'email' => 'tanil@cebv.com',
-        ]);
-
-        User::factory()->create([
-            'empleado_id' => Empleado::create([
-                'sexo_id' => 1,
-                'puesto_id' => 1,
-                'oficina_id' => Oficina::inRandomOrder()->first()->id,
-                'nombre' => 'Armando',
-                'apellido_paterno' => 'Luna',
-                'apellido_materno' => 'Sanchez',
-            ])->id,
-            'email' => 'armando@cebv.com',
-        ]);
-
-        User::factory()->create([
-            'empleado_id' => Empleado::create([
-                'sexo_id' => 1,
-                'puesto_id' => 1,
-                'oficina_id' => Oficina::inRandomOrder()->first()->id,
-                'nombre' => 'Luz',
-                'apellido_paterno' => 'Uribe',
-                'apellido_materno' => 'Vargas',
-            ])->id,
-            'email' => 'luz@cebv.com',
-        ]);
-
-        User::factory()->create([
-            'empleado_id' => Empleado::create([
-                'sexo_id' => 1,
-                'puesto_id' => 1,
-                'oficina_id' => Oficina::inRandomOrder()->first()->id,
-                'nombre' => 'Luis',
-                'apellido_paterno' => 'Arguello',
-                'apellido_materno' => 'Hernandez',
-            ])->id,
-            'email' => 'luis@cebv.com',
+            'email' => 'tanil@example.com',
         ]);
     }
 }
